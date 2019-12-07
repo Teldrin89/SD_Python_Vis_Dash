f41d85f2b22802e337d59add086abd6befdc976e
Lucas Cieslak <lukasz.cieslak69@gmail.com>
2019-12-08 00:45:49 +0100

    1st run of dash app with 2 html elements and some basic chart options

--- dash_tut1.py
+++ dash_tut1.py
@@ -14,9 +14,43 @@ The layout of the application can be edited with .css files
 app = dash.Dash()
 
 # to setup the layout
-app.layout = html.Div("Dash Tutorial")
+# app.layout = html.Div("Dash Tutorial")
+# html div can store a list of html tags
+app.layout = html.Div(children=[
+    # each child is within new line = first is h1 header tag
+    html.H1('Dash tutorials'),
+    # the chart is invoked using the dcc elemnts - Graph
+    dcc.Graph(id='example',
+        # figure data, layout and other properties are stored below
+        figure={
+            'data' : [
+                # explored different types of charts - line, point
+                {'x':[1,2,3,4,5], 'y':[200, 400, 230, 300, 350],
+                    'type':'line', 'name':'boats'},
+                {'x':[1,2,3,4,5], 'y':[100, 230, 130, 150, 180],
+                    'type':'line', 'name':'cars'},
+                {'x':[3.5,5], 'y':[250,250],
+                    # played a bit with customization of chart series
+                    'mode':'markers', 'name':'points', 'marker': {
+                        'size':12,
+                        'symbol':'hexagon2'
+                    }}
+                ],
+            'layout': {
+                'title':'Example of simple line chart',
+                # adjusted a bit layout - froze the yaxis with specific range
+                'yaxis': {
+                    'title': {
+                        'text':'Units'
+                    },
+                    'range':[0, 500],
+                    'fixedrange':True
+                }
+            }
+        })])
 
 # to run the app with debuger
 if __name__ == '__main__':
     app.run_server(debug=True)
-    
+
+# dash application will be run on the computer it's on with port 8050
